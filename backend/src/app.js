940ba7f947c53947ac2665fbbd6172cfdc523009
Gabriel Guimarães Vilas Boas Marin <gabrielvbmarin21@gmail.com>
2020-03-27 17:11:01 -0300

    backend validating and jest testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const app = express();
-app.use(express.json());
 app.use(cors());
-
+app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 console.log("System on port 3333");
-app.listen(3333);
+
+module.exports = app;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const crypto = require("crypto");
+const { celebrate, Segments, Joi } = require('celebrate');
 const connection = require('./database/connection');
 const ongControllers = require('./controllers/ongControllers');
 const incidentControllers= require('./controllers/incidentControllers');
@@ -8,14 +9,43 @@ const sessionController = require('./controllers/sessionController');
 
 const routes = express.Router();
 
-routes.post('/ongs', ongControllers.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        whatsapp: Joi.string().min(10).max(11).required(),
+        city: Joi.string().required(),
+        uf: Joi.string().length(2).required()
+    })
+}), ongControllers.create);
+
 routes.get('/ongs', ongControllers.getAll);
 
-routes.post('/incidents', incidentControllers.create);
-routes.get('/incidents', incidentControllers.getAll);
-routes.delete('/incidents/:id', incidentControllers.deleteOne);
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number()
+    })
+}), incidentControllers.create);
 
-routes.get('/profile', profileControllers.listIncidents);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), incidentControllers.getAll);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), incidentControllers.deleteOne);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), profileControllers.listIncidents);
 
 routes.post('/sessions', sessionController.createSession);
 
